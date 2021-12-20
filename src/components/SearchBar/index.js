c1f84dbf1fd61e4131fb67e5ce294fc6461eb203
ecp-alex <alex.mackenzie@englishcollege.cz>
2021-12-20 17:55:44 +0100

    TEST | test netlify auto-build change

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -29,7 +29,7 @@ const SearchBar = ({ setSearchTerm}) => {
                 <img src={searchIcon} alt='search-icon' />
                 <input
                 type='text'
-                placeholder='Search Movie'
+                placeholder='Search Movies'
                 onChange={e => setState(e.currentTarget.value)}
                 value={state}
                 />
